8998a740dc552001afe7e2bed7c2f0a487d4dddb
Alex Dias <aalexdias.010@gmail.com>
2020-08-14 23:14:33 -0300

    eleicoes otimizadas

--- aed_2/merge_sort/eleicoes_otimizadas.cpp
+++ aed_2/merge_sort/eleicoes_otimizadas.cpp
@@ -1,23 +1,106 @@
 #include <iostream>
+#include <cstdlib>
+
+#define NUM_OF_ROUNDS 3
 
 using namespace std;
 
+void print_aray(int[], int);
 void merge_sort(int[], int, int);
 void merge(int[], int, int, int);
+void start_election(int, int);
+int  validate_vote(int, int);
+int  perform_first_round(int[], int);
+int  perform_second_round(int[], int[], int[], int);
 
 int main() {
-  int ary[] = {9,5,4,2,8,7,3,6,1};
+  int num_of_electors, num_of_candidates;
 
-  merge_sort(ary, 0, 8);
+  cin >> num_of_electors;
+  cin >> num_of_candidates;
 
-  for (int i = 0; i < 9; i++)
-    cout << ary[i] << " ";
+  start_election(num_of_electors, num_of_candidates);
 
   return 0;
 }
 
-void merge_sort(int ary[], int l_index, int r_index)
-{
+void print_aray(int ary[], int size) {
+  cout << endl;
+  for (int i = 0; i < size; i++)
+    cout << ary[i] << " ";
+}
+
+void start_election(int num_of_electors, int num_of_candidates) {
+  int first_vote, second_vote, third_vote;
+
+  int *first_option_votes = new int[num_of_electors*sizeof(int)];
+  int *sec_option_votes   = new int[num_of_electors*sizeof(int)];
+  int *third_option_votes = new int[num_of_electors*sizeof(int)];
+
+  for (int i = 0; i < num_of_electors; i++) {
+    cin >> first_vote;
+    cin >> second_vote;
+    cin >> third_vote;
+
+    first_option_votes[i] = validate_vote(first_vote,  num_of_candidates);
+    sec_option_votes[i]   = validate_vote(second_vote, num_of_candidates);
+    third_option_votes[i] = validate_vote(third_vote,  num_of_candidates);
+  }
+
+  merge_sort(first_option_votes, 0, (num_of_electors - 1));
+  merge_sort(sec_option_votes,   0, (num_of_electors - 1));
+  merge_sort(third_option_votes, 0, (num_of_electors - 1));
+
+  if(!perform_first_round(first_option_votes, num_of_electors))
+    perform_second_round(
+      first_option_votes,
+      sec_option_votes,
+      third_option_votes,
+      num_of_electors
+    );
+}
+
+int validate_vote(int vote, int num_of_candidates) {
+  vote > 0 && vote <= num_of_candidates ? vote : 0;
+}
+
+int perform_first_round(int f_option_votes[], int num_of_electors) {
+  int most_voted_candidate, total_votes = 0;
+  int tmp_most_voted_candidate = f_option_votes[0];
+
+  for (int i, tmp_total_votes = 1; i < num_of_electors; i++) {
+    if (f_option_votes[i] == tmp_most_voted_candidate) {
+      tmp_total_votes++;
+      continue;
+    }
+    else if (tmp_total_votes > total_votes) {
+      most_voted_candidate = tmp_most_voted_candidate;
+      total_votes = tmp_total_votes;
+    }
+
+    tmp_most_voted_candidate = f_option_votes[i];
+    tmp_total_votes = 1;
+  }
+
+  float percent_of_votes = 100*(total_votes / (float)num_of_electors);
+
+  if (most_voted_candidate == 0) {
+    cout << most_voted_candidate;
+    exit(0);
+  }
+
+  cout << fixed;
+  cout.precision(2);
+  cout << most_voted_candidate << " " << percent_of_votes;
+
+  return percent_of_votes >= 50.0 ? true : false;
+}
+
+int perform_second_round(int f_opt_votes[], int s_opt_votes[], int t_opt_votes[], int num_of_candidates) {
+  /* PROGRAMAR EM C É TOTALMENTE IMPRODUTIVO ATUALMENTE */
+}
+
+void merge_sort(int ary[], int l_index, int r_index) {
   if (l_index >= r_index) return;
 
   int m_index = (l_index + r_index) / 2;
